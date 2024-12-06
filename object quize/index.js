9daa59ad905a59694807e251859c06cfcf7f6bf7
Iqra khan . <ik22082002khan@gmail.com>
2024-12-06 20:11:27 +0530

    Create index.js

---
+++ object quize/index.js
@@ -0,0 +1,9 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+let student = {
+    "name":"Iqra khan",
+    "phone_number":987654321,
+    "Marks":98,
+}
+console.log(student);
